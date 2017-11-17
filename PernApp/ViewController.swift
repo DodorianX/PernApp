cf3040fee9259ee64e3a975988765ca97130a902
Andrea Mignano <andreamignano@10-20-50-67.0200.fed.it.iosda.org>
2017-11-17 14:37:50 +0100

    Initial Commit

---
+++ PernApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PernApp
+//
+//  Created by Andrea Mignano on 17/11/17.
+//  Copyright © 2017 Andrea Mignano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
